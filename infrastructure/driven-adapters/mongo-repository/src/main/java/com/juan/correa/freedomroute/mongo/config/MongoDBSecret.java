11cd41627b5f48c8a4214bf38a16042be9fb356b
Juangmcz <juangui-089@hotmail.com>
2023-03-31 14:12:22 -0500

    Driven Adapter was added.

---
+++ infrastructure/driven-adapters/mongo-repository/src/main/java/com/juan/correa/freedomroute/mongo/config/MongoDBSecret.java
@@ -0,0 +1,11 @@
+package com.juan.correa.freedomroute.mongo.config;
+
+import lombok.Builder;
+import lombok.Getter;
+@Builder
+@Getter
+public class MongoDBSecret {
+private final String uri;
+
+
+}
