68b73325d64029b68479e496f2cd96c73e574ccb
Juangmcz <juangui-089@hotmail.com>
2023-03-31 12:44:30 -0500

    Clean architecture structure was added.

---
+++ applications/app-service/src/main/java/com/juan/correa/freedomroute/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package com.juan.correa.freedomroute.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "com.juan.correa.freedomroute.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
