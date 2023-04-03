979fa86b6e20fab13b32bdd6980d4fbbb76eb5a3
Juangmcz <juangui-089@hotmail.com>
2023-04-03 07:32:31 -0500

    BusTicketRepository entity was implemented.

--- domain/model/src/main/java/com/juan/correa/freedomroute/model/busticket/gateways/BusTicketRepository.java
+++ domain/model/src/main/java/com/juan/correa/freedomroute/model/busticket/gateways/BusTicketRepository.java
@@ -1,4 +1,14 @@
 package com.juan.correa.freedomroute.model.busticket.gateways;
 
+import com.juan.correa.freedomroute.model.busticket.BusTicket;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 public interface BusTicketRepository {
+
+    Flux<BusTicket> getAllBusTickets();
+    Mono<BusTicket> getBusTicketById(String id);
+    Mono<BusTicket> saveBusTicket(BusTicket busTicket);
+    Mono<BusTicket> updateBusTicket(String id, BusTicket busTicket);
+    Mono<Void> deleteBusTicket(String id);
 }
