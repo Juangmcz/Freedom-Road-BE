b19a84b75b0695755aa4f6c674aa63bef53ea7f4
Juangmcz <juangui-089@hotmail.com>
2023-04-03 07:32:50 -0500

    PurchaseOrderRepository entity was implemented.

--- domain/model/src/main/java/com/juan/correa/freedomroute/model/purchaseorder/gateways/PurchaseOrderRepository.java
+++ domain/model/src/main/java/com/juan/correa/freedomroute/model/purchaseorder/gateways/PurchaseOrderRepository.java
@@ -1,4 +1,14 @@
 package com.juan.correa.freedomroute.model.purchaseorder.gateways;
 
+import com.juan.correa.freedomroute.model.purchaseorder.PurchaseOrder;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 public interface PurchaseOrderRepository {
+
+    Flux<PurchaseOrder> getAllPurchaseOrders();
+    Mono<PurchaseOrder> getPurchaseOrderById(String id);
+    Mono<PurchaseOrder> savePurchaseOrder(PurchaseOrder purchaseOrder);
+    Mono<PurchaseOrder> updatePurchaseOrder(String id, PurchaseOrder purchaseOrder);
+    Mono<Void> deletePurchaseOrder(String id);
 }
