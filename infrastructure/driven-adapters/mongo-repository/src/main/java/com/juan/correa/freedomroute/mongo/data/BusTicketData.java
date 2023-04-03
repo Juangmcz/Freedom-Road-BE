6dd0c4c068c2828038168b61719d950b84da2947
Juangmcz <juangui-089@hotmail.com>
2023-04-03 14:39:46 -0500

    UUID was implemented.

--- infrastructure/driven-adapters/mongo-repository/src/main/java/com/juan/correa/freedomroute/mongo/data/BusTicketData.java
+++ infrastructure/driven-adapters/mongo-repository/src/main/java/com/juan/correa/freedomroute/mongo/data/BusTicketData.java
@@ -8,13 +8,15 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.UUID;
+
 @Data
 @Document(collection = "bustickets")
 @NoArgsConstructor
 @AllArgsConstructor
 public class BusTicketData {
     @Id
-    private String id;
+    private String id = UUID.randomUUID().toString().substring(0,10);
     @NotBlank(message="Empty field error")
     @NotNull(message ="Origin field is required")
     private String origin;
--- infrastructure/driven-adapters/mongo-repository/src/main/java/com/juan/correa/freedomroute/mongo/data/PurchaseOrderData.java
+++ infrastructure/driven-adapters/mongo-repository/src/main/java/com/juan/correa/freedomroute/mongo/data/PurchaseOrderData.java
@@ -8,13 +8,15 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.UUID;
+
 @Data
 @Document(collection = "purchaseorders")
 @NoArgsConstructor
 @AllArgsConstructor
 public class PurchaseOrderData {
     @Id
-    private String id;
+    private String id = UUID.randomUUID().toString().substring(0,10);
     @NotBlank(message="Empty field error")
     @NotNull(message ="User ID field is required")
     private String userId;
