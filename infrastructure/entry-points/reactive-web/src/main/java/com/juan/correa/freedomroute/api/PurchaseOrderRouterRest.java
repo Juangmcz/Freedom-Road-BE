f0966ec913b1f257bb1663ba1ab27b25854619b2
Juangmcz <juangui-089@hotmail.com>
2023-04-03 16:57:42 -0500

    @Configuration annotation was added.

--- infrastructure/entry-points/reactive-web/src/main/java/com/juan/correa/freedomroute/api/PurchaseOrderRouterRest.java
+++ infrastructure/entry-points/reactive-web/src/main/java/com/juan/correa/freedomroute/api/PurchaseOrderRouterRest.java
@@ -7,6 +7,7 @@ import com.juan.correa.freedomroute.usecase.purchaseorders.getpurchaseorderbyid.
 import com.juan.correa.freedomroute.usecase.purchaseorders.savepurchaseorder.SavePurchaseOrderUseCase;
 import com.juan.correa.freedomroute.usecase.purchaseorders.updatepurchaseorder.UpdatePurchaseOrderUseCase;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.BodyInserters;
@@ -18,6 +19,7 @@ import static org.springframework.web.reactive.function.server.RequestPredicates
 import static org.springframework.web.reactive.function.server.RequestPredicates.DELETE;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
+@Configuration
 public class PurchaseOrderRouterRest {
 
     @Bean
