44e92df4264b03bb571abd3d9e997b7372be5d68
Juangmcz <juangui-089@hotmail.com>
2023-04-03 08:33:57 -0500

    DeleteBusTicketUseCase was implemented.

--- domain/usecase/src/main/java/com/juan/correa/freedomroute/usecase/bustickets/deletebusticket/DeleteBusTicketUseCase.java
+++ domain/usecase/src/main/java/com/juan/correa/freedomroute/usecase/bustickets/deletebusticket/DeleteBusTicketUseCase.java
@@ -1,6 +1,18 @@
 package com.juan.correa.freedomroute.usecase.bustickets.deletebusticket;
 
+import com.juan.correa.freedomroute.model.busticket.gateways.BusTicketRepositoryGateway;
 import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
+import java.util.function.Function;
+
 @RequiredArgsConstructor
-public class DeleteBusTicketUseCase {
+public class DeleteBusTicketUseCase implements Function<String, Mono<Void>> {
+
+    private final BusTicketRepositoryGateway repositoryGateway;
+
+    @Override
+    public Mono<Void> apply(String busTicketId) {
+        return repositoryGateway.deleteBusTicket(busTicketId);
+    }
 }
