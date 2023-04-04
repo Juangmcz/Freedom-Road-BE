95e1cd66f6d45439c90bf01160d2315279018dd0
Juangmcz <juangui-089@hotmail.com>
2023-04-04 16:27:14 -0500

    Swagger documentation was implemented.

--- infrastructure/entry-points/reactive-web/src/main/java/com/juan/correa/freedomroute/api/PurchaseOrderRouterRest.java
+++ infrastructure/entry-points/reactive-web/src/main/java/com/juan/correa/freedomroute/api/PurchaseOrderRouterRest.java
@@ -6,10 +6,19 @@ import com.juan.correa.freedomroute.usecase.purchaseorders.getallpurchaseorders.
 import com.juan.correa.freedomroute.usecase.purchaseorders.getpurchaseorderbyid.GetPurchaseOrderByIdUseCase;
 import com.juan.correa.freedomroute.usecase.purchaseorders.savepurchaseorder.SavePurchaseOrderUseCase;
 import com.juan.correa.freedomroute.usecase.purchaseorders.updatepurchaseorder.UpdatePurchaseOrderUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springdoc.core.annotations.RouterOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -23,6 +32,20 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.r
 public class PurchaseOrderRouterRest {
 
     @Bean
+    @RouterOperation(path = "/api/purchaseorders",
+            produces = {MediaType.APPLICATION_JSON_VALUE},
+            beanClass = GetAllPurchaseOrdersUseCase.class,
+            method = RequestMethod.GET,
+            beanMethod = "get",
+            operation = @Operation(operationId = "getAllPurchaseOrders",
+                    tags = "Purchase Order Use Cases",
+                    responses = {
+                    @ApiResponse(responseCode = "200",
+                            description = "Success",
+                            content = @Content(schema = @Schema(implementation = PurchaseOrder.class))),
+                    @ApiResponse(responseCode = "204", description = "No orders found")}
+            )
+    )
     public RouterFunction<ServerResponse> getAllPurchaseOrders(GetAllPurchaseOrdersUseCase getAllPurchaseOrdersUseCase){
         return route(GET("/api/purchaseorders"),
                 request -> ServerResponse.status(201)
@@ -32,6 +55,37 @@ public class PurchaseOrderRouterRest {
     }
 
     @Bean
+    @RouterOperation(path = "/api/purchaseorders/{id}",
+            produces = {MediaType.APPLICATION_JSON_VALUE},
+            beanClass = GetPurchaseOrderByIdUseCase.class,
+            method = RequestMethod.GET,
+            beanMethod = "apply",
+            operation = @Operation(operationId = "getPurchaseOrderById",
+                    tags = "Purchase Order Use Cases",
+                    parameters = {
+                            @Parameter(
+                                    name = "id",
+                                    description = "Purchase Order Id",
+                                    required = true,
+                                    in = ParameterIn.PATH
+                            )
+                    },
+                    responses = {
+                            @ApiResponse(
+                                    responseCode = "200",
+                                    description = "Purchase order found",
+                                    content = @Content(
+                                            schema = @Schema(
+                                                    implementation = PurchaseOrder.class
+                                            )
+                                    )
+                            ),
+                            @ApiResponse(responseCode = "404",
+                                    description = "Order not found"
+                            )
+                    }
+            )
+    )
     public RouterFunction<ServerResponse> getPurchaseOrderById(GetPurchaseOrderByIdUseCase getPurchaseOrderByIdUseCase){
         return route(GET("/api/purchaseorders/{id}"),
                 request -> getPurchaseOrderByIdUseCase.apply(request.pathVariable("id"))
@@ -43,6 +97,33 @@ public class PurchaseOrderRouterRest {
     }
 
     @Bean
+    @RouterOperation(path = "/api/purchaseorders",
+            produces = {MediaType.APPLICATION_JSON_VALUE},
+            beanClass = SavePurchaseOrderUseCase.class,
+            method = RequestMethod.POST,
+            beanMethod = "apply",
+            operation = @Operation(
+                    operationId = "savePurchaseOrder",
+                    tags = "Purchase Order Use Cases",
+                    parameters = {
+                            @Parameter(
+                                    name = "purchaseorder",
+                                    in = ParameterIn.PATH,
+                                    schema = @Schema(implementation = PurchaseOrder.class)
+                            )
+                    },
+                    responses = {
+                            @ApiResponse(
+                                    responseCode = "201",
+                                    description = "Success",
+                                    content = @Content(schema = @Schema(implementation = PurchaseOrder.class))
+                            ),
+                            @ApiResponse(responseCode = "406", description = "Not acceptable")},
+                    requestBody = @RequestBody(required = true,
+                            description = "Save a purchase order",
+                            content = @Content(schema = @Schema(implementation = PurchaseOrder.class)))
+            )
+    )
     public RouterFunction<ServerResponse> savePurchaseOrder(SavePurchaseOrderUseCase savePurchaseOrderUseCase) {
         return route(POST("/api/purchaseorders").and(accept(MediaType.APPLICATION_JSON)),
                 request -> request.bodyToMono(PurchaseOrder.class)
@@ -54,6 +135,41 @@ public class PurchaseOrderRouterRest {
     }
 
     @Bean
+    @RouterOperation(path = "/api/purchaseorders/{id}",
+            produces = {MediaType.APPLICATION_JSON_VALUE},
+            beanClass = UpdatePurchaseOrderUseCase.class,
+            method = RequestMethod.PUT,
+            beanMethod = "apply",
+            operation = @Operation(
+                    operationId = "updatePurchaseOrder",
+                    tags = "Purchase Order Use Cases",
+                    parameters = {
+                            @Parameter(
+                                    name = "id",
+                                    description = "Purchase order Id",
+                                    required = true,
+                                    in = ParameterIn.PATH
+                            ),
+                            @Parameter(
+                                    name = "purchaseorder",
+                                    in = ParameterIn.PATH,
+                                    schema = @Schema(implementation = PurchaseOrder.class)
+                            )
+                    },
+                    responses = {
+                            @ApiResponse(
+                                    responseCode = "201",
+                                    description = "Success",
+                                    content = @Content(schema = @Schema(implementation = PurchaseOrder.class))
+                            ),
+                            @ApiResponse(responseCode = "406", description = "Not acceptable")},
+                    requestBody = @RequestBody(
+                            required = true,
+                            description = "Update a purchase order",
+                            content = @Content(schema = @Schema(implementation = PurchaseOrder.class))
+                    )
+            )
+    )
     public RouterFunction<ServerResponse> updatePurchaseOrder(UpdatePurchaseOrderUseCase updatePurchaseOrderUseCase) {
         return route(PUT("/api/purchaseorders/{id}").and(accept(MediaType.APPLICATION_JSON)),
                 request -> request.bodyToMono(PurchaseOrder.class)
@@ -66,6 +182,37 @@ public class PurchaseOrderRouterRest {
     }
 
     @Bean
+    @RouterOperation(path = "/api/purchaseorders/{id}",
+            produces = {MediaType.APPLICATION_JSON_VALUE},
+            beanClass = DeletePurchaseOrderUseCase.class,
+            method = RequestMethod.DELETE,
+            beanMethod = "apply",
+            operation = @Operation(operationId = "deletePurchaseOrder",
+                    tags = "Purchase Order Use Cases",
+                    parameters = {
+                            @Parameter(
+                                    name = "id",
+                                    description = "Purchase Order Id",
+                                    required = true,
+                                    in = ParameterIn.PATH
+                            )
+                    },
+                    responses = {
+                            @ApiResponse(
+                                    responseCode = "200",
+                                    description = "Purchase order deleted successfully",
+                                    content = @Content(
+                                            schema = @Schema(
+                                                    implementation = PurchaseOrder.class
+                                            )
+                                    )
+                            ),
+                            @ApiResponse(responseCode = "404",
+                                    description = "Order not found"
+                            )
+                    }
+            )
+    )
     public RouterFunction<ServerResponse> deletePurchaseOrder(DeletePurchaseOrderUseCase deletePurchaseOrderUseCase){
         return route(DELETE("/api/purchaseorders/{id}"),
                 request ->  deletePurchaseOrderUseCase.apply(request.pathVariable("id"))
