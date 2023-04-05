5decce00f78b6842b537195709f5628df56e7e22
Juangmcz <juangui-089@hotmail.com>
2023-04-05 10:31:15 -0500

    SaveCustomerUseCase test was implemented.

---
+++ domain/usecase/src/test/java/com/juan/correa/freedomroute/usecase/customers/savecustomer/SaveCustomerUseCaseTest.java
@@ -0,0 +1,42 @@
+package com.juan.correa.freedomroute.usecase.customers.savecustomer;
+
+import com.juan.correa.freedomroute.model.customer.Customer;
+import com.juan.correa.freedomroute.model.customer.gateways.CustomerRepositoryGateway;
+import com.juan.correa.freedomroute.usecase.DataMocks;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.test.StepVerifier;
+
+@ExtendWith(MockitoExtension.class)
+class SaveCustomerUseCaseTest {
+
+    @Mock
+    CustomerRepositoryGateway customerRepository;
+
+    SaveCustomerUseCase saveCustomerUseCase;
+
+    @BeforeEach
+    void init(){
+        saveCustomerUseCase = new SaveCustomerUseCase(customerRepository);
+    }
+
+    @Test
+    @DisplayName("saveCustomer_success")
+    void saveCustomer(){
+
+        Mockito.when(customerRepository.saveCustomer(ArgumentMatchers.any(Customer.class))).thenReturn(DataMocks.customerById());
+
+        var result = saveCustomerUseCase.apply(DataMocks.customer());
+
+        StepVerifier.create(result)
+                .expectNextMatches(customer -> customer.getId().equals("1"))
+                .verifyComplete();
+        Mockito.verify(customerRepository).saveCustomer(ArgumentMatchers.any(Customer.class));
+    }
+}
