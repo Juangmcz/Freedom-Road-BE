5feeb04919da67be5e06259c66a726afe1417efa
Juangmcz <juangui-089@hotmail.com>
2023-04-06 09:26:59 -0500

    Dockerfile and docker-compose were implemented.

--- applications/app-service/src/main/java/com/juan/correa/freedomroute/MainApplication.java
+++ applications/app-service/src/main/java/com/juan/correa/freedomroute/MainApplication.java
@@ -6,7 +6,7 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "Example CA API", version = "1.0", description = "Documentation APIs v1.0"))
+@OpenAPIDefinition(info = @Info(title = "Freedom Route API", version = "1.0", description = "Documentation APIs v1.0"))
 public class MainApplication {
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
